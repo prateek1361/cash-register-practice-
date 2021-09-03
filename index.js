21a2f56588ed38b029445f9f1f8c530157bd5836
prateek1361 <88934705+prateek1361@users.noreply.github.com>
2021-09-03 18:43:51 +0530

    commit 2

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
 
-    if(billAmount.value > 0) {
+    
         if(billAmount.value > 0){
         if(cashGiven.value >= billAmount.value){
-            cons amountToBeReturned = cashGiven.value - billAmount.value;
+            const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned); 
         }else{
             showMessage("The cash provided should atleast be equal to the bill amount");
         }
     }else{
-        showMessage(Invalid Bill Amount)
+        showMessage("Invalid Bill Amount")
        
     }
 });
